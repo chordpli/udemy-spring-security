6cedcaee504271cd247751d78f16a73d56948a8d
chordpli <chordplaylist@gmail.com>
2024-01-18 22:41:37 +0900

    init project

---
+++ src/main/java/com/pli/udemysecurity/UdemySecurityApplication.java
@@ -0,0 +1,13 @@
+package com.pli.udemysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySecurityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pli/udemysecurity/UdemySecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pli.udemysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemySecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
