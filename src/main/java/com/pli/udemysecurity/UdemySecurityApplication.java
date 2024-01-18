1b9df1fdbc6223afe991aa6b220ea21fed559c5b
chordpli <chordplaylist@gmail.com>
2024-01-18 22:43:51 +0900

    style: apply spotless

--- src/main/java/com/pli/udemysecurity/UdemySecurityApplication.java
+++ src/main/java/com/pli/udemysecurity/UdemySecurityApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UdemySecurityApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(UdemySecurityApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(UdemySecurityApplication.class, args);
+  }
 }
