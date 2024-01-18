7c8f4362138ba3c36ae77c20d3b39a1cd060b6bc
chordpli <chordplaylist@gmail.com>
2024-01-18 23:02:04 +0900

    feat: 보안 없이 Spring Boot 앱 구현

--- src/main/java/com/pli/udemysecurity/UdemySecurityApplication.java
+++ src/main/java/com/pli/udemysecurity/UdemySecurityApplication.java
@@ -2,8 +2,10 @@ package com.pli.udemysecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.pli.udemysecurity.controller")
 public class UdemySecurityApplication {
 
   public static void main(String[] args) {
