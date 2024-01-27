8d307399e00c4b442d34aec143103678cc569956
chordpli <chordplaylist@gmail.com>
2024-01-27 09:33:54 +0900

    feat: jdbc 인증 방법으로 변경

--- src/main/java/com/pli/udemysecurity/config/SecurityConfig.java
+++ src/main/java/com/pli/udemysecurity/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package com.pli.udemysecurity.config;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,14 +30,19 @@ public class SecurityConfig {
         .build();
   }
 
+  //  @Bean
+  //  public InMemoryUserDetailsManager userDetailsService() {
+  //
+  //    UserDetails admin =
+  // User.withUsername("admin").password("1234").authorities("ADMIN").build();
+  //    UserDetails user = User.withUsername("user").password("1234").authorities("read").build();
+  //
+  //    return new InMemoryUserDetailsManager(admin, user);
+  //  }
+
   @Bean
-  public InMemoryUserDetailsManager userDetailsService() {
-
-    UserDetails admin = User.withUsername("admin").password("1234").authorities("ADMIN").build();
-
-    UserDetails user = User.withUsername("user").password("1234").authorities("read").build();
-
-    return new InMemoryUserDetailsManager(admin, user);
+  public UserDetailsService userDetailsService(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
   }
 
   @Bean
