49a8268c967fad4acc9940bbdf3c0f76bee30984
chordpli <chordplaylist@gmail.com>
2024-02-01 23:04:06 +0900

    feat: 모델 생성

--- src/main/java/com/pli/udemysecurity/model/Customer.java
+++ src/main/java/com/pli/udemysecurity/model/Customer.java
@@ -1,50 +1,86 @@
 package com.pli.udemysecurity.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Customer {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+  @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+  @GenericGenerator(name = "native", strategy = "native")
+  @Column(name = "customer_id")
+  private int id;
+
+  private String name;
 
   private String email;
-  private String password;
-  private String roles;
 
-  public Long getId() {
+  @Column(name = "mobile_number")
+  private String mobileNumber;
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private String pwd;
+
+  private String role;
+
+  @Column(name = "create_dt")
+  private String createDt;
+
+  public int getId() {
     return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
   }
 
   public String getEmail() {
     return email;
   }
 
-  public String getPassword() {
-    return password;
-  }
-
-  public String getRoles() {
-    return roles;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
   public void setEmail(String email) {
     this.email = email;
   }
 
-  public void setPassword(String password) {
-    this.password = password;
+  public String getMobileNumber() {
+    return mobileNumber;
   }
 
-  public void setRoles(String roles) {
-    this.roles = roles;
+  public void setMobileNumber(String mobileNumber) {
+    this.mobileNumber = mobileNumber;
+  }
+
+  public String getPwd() {
+    return pwd;
+  }
+
+  public void setPwd(String pwd) {
+    this.pwd = pwd;
+  }
+
+  public String getRole() {
+    return role;
+  }
+
+  public void setRole(String role) {
+    this.role = role;
+  }
+
+  public String getCreateDt() {
+    return createDt;
+  }
+
+  public void setCreateDt(String createDt) {
+    this.createDt = createDt;
   }
 }
