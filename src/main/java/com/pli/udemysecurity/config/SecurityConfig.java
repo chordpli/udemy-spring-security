c82b18e5f239b9966cbcb43abd32caa43e2368a5
chordpli <chordplaylist@gmail.com>
2024-02-03 18:53:58 +0900

    feat: 무시할 csrf 설정.
    post api중 공공적으로 사용될 api 또는 인증 인가가 필요하지 않은 경우 csrf 공격 무시

--- src/main/java/com/pli/udemysecurity/config/SecurityConfig.java
+++ src/main/java/com/pli/udemysecurity/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,7 +28,11 @@ public class SecurityConfig {
                     .permitAll())
         .httpBasic(Customizer.withDefaults())
         .formLogin(Customizer.withDefaults())
-        .csrf(AbstractHttpConfigurer::disable)
+        .csrf(
+            httpSecurityCsrfConfigurer ->
+                httpSecurityCsrfConfigurer.ignoringRequestMatchers(
+                    (request) -> "/contact".equals(request.getRequestURI()),
+                    (request) -> "/register".equals(request.getRequestURI())))
         .build();
   }
 
