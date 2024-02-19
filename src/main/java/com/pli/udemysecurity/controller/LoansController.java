007bc5079007a87ed64d1de241c945951eb55863
chordpli <chordplaylist@gmail.com>
2024-02-19 22:47:00 +0900

    feat: @PostAuthorize 사용

--- src/main/java/com/pli/udemysecurity/controller/LoansController.java
+++ src/main/java/com/pli/udemysecurity/controller/LoansController.java
@@ -1,13 +1,21 @@
 package com.pli.udemysecurity.controller;
 
+import com.pli.udemysecurity.model.Loans;
+import com.pli.udemysecurity.repository.LoanRepository;
+import java.util.List;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoansController {
 
+  private LoanRepository loanRepository;
+
   @GetMapping("/myLoans")
-  public String getLoans() {
-    return "Here are the loans details";
+  @PostAuthorize("hasRole('ROOT')")
+  public List<Loans> getLoanDetails(@RequestParam int id) {
+    return loanRepository.findByCustomerIdOrderByStartDtDesc(id);
   }
 }
