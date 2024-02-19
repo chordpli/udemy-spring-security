60ec3e08ea617e7e52ce905cca6e1987b33d4566
chordpli <chordplaylist@gmail.com>
2024-02-19 22:43:26 +0900

    feat: 메서드 레벨 보안 설정

--- src/main/java/com/pli/udemysecurity/repository/LoanRepository.java
+++ src/main/java/com/pli/udemysecurity/repository/LoanRepository.java
@@ -3,10 +3,12 @@ package com.pli.udemysecurity.repository;
 import com.pli.udemysecurity.model.Loans;
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LoanRepository extends CrudRepository<Loans, Long> {
 
+  @PreAuthorize("hasRole('User')")
   List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
 }
